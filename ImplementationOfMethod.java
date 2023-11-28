2940673c2276b7ae89ad8b6bd3608abd188dfdbd
rupalee kumari <rupaleekumari152@gmail.com>
2023-11-28 07:33:58 +0000

    third msg

---
+++ ImplementationOfMethod.java
@@ -0,0 +1,27 @@
+package com;
+public class ImplementationOfMethod {
+	//method to calculate sum of two numbers -num1&num2
+	 public static int sum(int num1,int num2) {
+		 //public static void sum(num1,num2) 
+			 
+		 
+		int sum=num1+num2;
+		return sum;
+		//System.out.println(sum)
+	
+	}
+
+public static void main(String[] args) {
+	
+		System.out.println(sum(2,7));
+//		 num1=2;
+//		num2=7;
+		//sum(num1 , num2) function call
+		
+		}
+
+	}
+
+
+
+
